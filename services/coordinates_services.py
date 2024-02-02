1a5e6828de3c106b3730d64248c5966117a83c71
mpruvot <marius.pruvot@outlook.fr>
2024-02-02 06:32:45 +0100

    reoarder lat & long to match the gps format in adress_to_gps

--- services/coordinates_services.py
+++ services/coordinates_services.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from pyproj import Transformer
 
@@ -11,30 +13,39 @@ def lamber93_to_gps(x: int, y: int) -> tuple:
     return long, lat
 
 
-def gps_to_lamber93(long: float, lat: float) -> tuple:
-    """Convert GPS coordinates (longitude, latitude) to Lambert 93 coordinates"""
-
+def gps_to_lamber93(lat: float, long: float) -> tuple:
+    """Convert GPS coordinates (latitude, longitude) to Lambert93 coordinates and round to integers"""
     transformer = Transformer.from_crs("EPSG:4326", "EPSG:2154")
     x, y = transformer.transform(long, lat)
 
-    return x, y
+    return int(round(x)), int(round(y))
 
 
 def adress_to_gps(adress: str) -> tuple:
     """Convert adress to GPS coordinates (longitude, latitude)"""
-    api_url = "https://api-adresse.data.gouv.fr/search/?q="
+    API_URL = "https://api-adresse.data.gouv.fr/search/"
 
     try:
-        response = requests.get(api_url + adress)
+        response = requests.get(
+            API_URL,
+            params={
+                "q": adress,
+                "limit": 1,
+                "autocomplete": 0,
+            },
+        )
         response.raise_for_status()
-        response_json = response.json()
-        long = response_json["features"][0]["geometry"]["coordinates"][0]
-        lat = response_json["features"][0]["geometry"]["coordinates"][1]
-
-        return long, lat
+        data = response.json()
+        print(json.dumps(data, indent=4))
+        long = data["features"][0]["geometry"]["coordinates"][0]
+        lat = data["features"][0]["geometry"]["coordinates"][1]
+        return lat, long
 
     except requests.exceptions.HTTPError as err:
         raise requests.exceptions.HTTPError(err)
 
     except IndexError as err:
         raise IndexError(err)
+
+
+print(adress_to_gps("17,rue du delta 75009 paris"))
