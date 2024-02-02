6c7e1de279dc057fc74a52e2c172691e8a73565a
mpruvot <marius.pruvot@outlook.fr>
2024-02-02 09:27:33 +0100

    edit docstring

--- models/coverage_model.py
+++ models/coverage_model.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class NetworkCoverage(BaseModel):
-    """Network coverage model, representing the network coverage of the 3 main French operator"""
+    """Network coverage model, representing the network coverage of the 4 main French operator"""
 
     Orange: Optional[Dict] = Field(None, description="Orange network coverage")
     Bouygues: Optional[Dict] = Field(None, description="Bouygues network coverage")
